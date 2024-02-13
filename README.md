f574df03f63203596c6d1e86d0cd861b336f3abe
sauvtc <sauvtc@gmail.com>
2024-02-13 19:38:14 +0700

    refactor the main conde and green 4 forever

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 ### You will find in thí repo the following stuff:
 * The Math-Util library that offers multible math funcions (a Java-based source code)
 * Unit Testing source code with Junit framework
-* ...
+* GitHub Actions to demonstrate the Continuous Integration (CI) technique
+* Regression test
 ### Screen shots
 ![DDT & TDD with Junit](https://github.com/sauvtc/math-util-1/blob/main/images/mycode.png)
 #### © 2024 sower
--- src/com/ilovecode/mathutil/core/MathUtil.java
+++ src/com/ilovecode/mathutil/core/MathUtil.java
@@ -19,15 +19,34 @@ public class MathUtil {
     // 20 giai thừa vừa kiểu long
     // 21 giai thừa tràn kiểu long
     //quy ước tính giai thừa từ 1-20
-    public static long getFactorial(int n){
-        if(n<0 || n>20)
-            throw new IllegalArgumentException("invalid argment n between 0-20");
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20)
+//            throw new IllegalArgumentException("invalid argment n between 0-20");
+//        if (n==0 ||n==1)
+//            return 1;// nhân đầu vào đặc biệt
+//        long product=1; //
+//        for (int i = 2; i <=n; i++) 
+//            product*=i;
+//        return product;
+//    }
+    //Tính giai thừa bằng đệ quy
+    // ví dụ tính giúp 6!
+    // n=1.2.3.4.5.6..n
+    // 6=6.5!
+    // 5=5.4!
+    // 4=4.3!
+    // 3=3.2!
+    // 2=2.1!
+    // 1=1;
+    // quy ươc 1!=0!=1;
+    // chốt hạ: n!=n*(n-1)!
+    
+      public static long getFactorial(int n){
+        if(n<0 || n>20) 
+            throw new IllegalArgumentException("n must be beetween 0--20Plz");
         if (n==0 ||n==1)
             return 1;// nhân đầu vào đặc biệt
-        long product=1; //
-        for (int i = 2; i <=n; i++) 
-            product*=i;
-        return product;
+        return n * getFactorial(n-1);//công thức đệ quy
     }
     
 }
